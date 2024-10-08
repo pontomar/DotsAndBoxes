2d2b73bc628b42a336d5303d031e043e2828f60e
ramaflow <pontonglang@protonmail.ch>
2024-10-08 11:44:03 +0200

    neighborCount

--- labs/lab04/GameOfLife.kt
+++ labs/lab04/GameOfLife.kt
@@ -13,8 +13,31 @@ fun main() {
         }
     }
     println(boardArray.contentDeepToString())
+    println(getNeighborCount(boardArray, 3,4))
 }
 
 fun assignRandomValue(): Int {
     return Random.nextInt(0, 2)
+}
+
+fun gameOfLife(){
+
+}
+fun getCellValue(boardArray:Array<Array<Int>>, x: Int, y: Int): Int {
+    return if (x in boardArray.indices && y in boardArray[x].indices) {
+        boardArray[x][y]
+    } else {
+        0 // If the index is out of bounds, return 0
+    }
+}
+
+fun getNeighborCount(boardArray: Array<Array<Int>>, currentFieldX: Int, currentFieldY: Int): Int {
+// Check each of the neighboring cells
+    var count = 0
+    count += getCellValue(boardArray, currentFieldX - 1, currentFieldY)   // North
+    count += getCellValue(boardArray, currentFieldX + 1, currentFieldY)   // South
+    count += getCellValue(boardArray, currentFieldX, currentFieldY - 1)   // West
+    count += getCellValue(boardArray, currentFieldX, currentFieldY + 1)   // East
+
+    return count
 }
