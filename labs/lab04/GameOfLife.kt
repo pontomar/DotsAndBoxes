30dc18c05a7593e056c37cde3c49123210fb7756
janneszuend <goalierebstei@gmail.com>
2024-10-08 18:47:51 +0200

    gameoflife vervollständigt

--- labs/lab04/GameOfLife.kt
+++ labs/lab04/GameOfLife.kt
@@ -1,38 +1,62 @@
 import kotlin.random.Random
 
 val boardArray = Array(10) { Array(10) { 0 } }
-val newBoardArray = Array(10) { Array(10) {0}}
+val newBoardArray = Array(10) { Array(10) { 0 } }
 
 fun main() {
-    // Accessing the top-level variable inside the main function
-    println(boardArray.contentDeepToString())
+    initializeBoardWithRandomOneOrZeros()
 
+    // Accessing the top-level variable inside the main function
+    println("Initial Board:")
+    prettyPrint2DArray(boardArray)
+    println("Actual Board State:")
+    prettyPrint2DArray(newBoardArray)
+
+}
+
+private fun initializeBoardWithRandomOneOrZeros() {
     for (i in boardArray.indices) { // Loop over the rows
         for (j in boardArray[i].indices) {
             boardArray[i][j] = assignRandomValue()
         }
     }
-    println(boardArray.contentDeepToString())
-    println(getNeighborCount(boardArray, 3,4))
 }
 
 fun assignRandomValue(): Int {
     return Random.nextInt(0, 2)
 }
 
-fun gameOfLife(){
-
+fun prettyPrint2DArray(array: Array<Array<Int>>) {
+    array.forEach { row ->
+        println(row.joinToString(" ") { it.toString() })
+    }
 }
-fun getCellValue(boardArray:Array<Array<Int>>, x: Int, y: Int): Int {
+
+private fun updateBoardState() {
+    for (i in boardArray.indices) { // Loop over the rows
+        for (j in boardArray[i].indices) {
+            var numOfNeighbours: Int = getNeighborCount(boardArray, i, j)
+
+            if (numOfNeighbours < 2) {
+                newBoardArray[i][j] = 0
+            } else if (numOfNeighbours > 3) {
+                newBoardArray[i][j] = 0
+            } else {
+                newBoardArray[i][j] = 1
+            }
+        }
+    }
+}
+
+fun getCellValue(boardArray: Array<Array<Int>>, x: Int, y: Int): Int {
     return if (x in boardArray.indices && y in boardArray[x].indices) {
         boardArray[x][y]
     } else {
-        0 // If the index is out of bounds, return 0
+        0
     }
 }
 
 fun getNeighborCount(boardArray: Array<Array<Int>>, currentFieldX: Int, currentFieldY: Int): Int {
-// Check each of the neighboring cells
     var count = 0
     count += getCellValue(boardArray, currentFieldX - 1, currentFieldY)   // North
     count += getCellValue(boardArray, currentFieldX + 1, currentFieldY)   // South
