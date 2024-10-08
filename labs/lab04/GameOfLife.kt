5fda98ca043f725130d290beb59fc216abf772f1
ramaflow <pontonglang@protonmail.ch>
2024-10-08 11:19:01 +0200

    random initial board values

--- labs/lab04/GameOfLife.kt
+++ labs/lab04/GameOfLife.kt
@@ -1,13 +1,20 @@
+import kotlin.random.Random
 
 val boardArray = Array(10) { Array(10) { 0 } }
-val newBoardArray = Array() { Array(10) {0}}
+val newBoardArray = Array(10) { Array(10) {0}}
 
 fun main() {
     // Accessing the top-level variable inside the main function
     println(boardArray.contentDeepToString())
 
-
-
+    for (i in boardArray.indices) { // Loop over the rows
+        for (j in boardArray[i].indices) {
+            boardArray[i][j] = assignRandomValue()
+        }
+    }
+    println(boardArray.contentDeepToString())
 }
 
-
+fun assignRandomValue(): Int {
+    return Random.nextInt(0, 2)
+}
