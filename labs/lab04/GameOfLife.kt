045e8d78d900b017545ea20f4f7c48dd8e4317f1
ramaflow <pontonglang@protonmail.ch>
2024-10-08 11:03:38 +0200

    the board

---
+++ labs/lab04/GameOfLife.kt
@@ -0,0 +1,13 @@
+
+val boardArray = Array(10) { Array(10) { 0 } }
+val newBoardArray = Array() { Array(10) {0}}
+
+fun main() {
+    // Accessing the top-level variable inside the main function
+    println(boardArray.contentDeepToString())
+
+
+
+}
+
+
