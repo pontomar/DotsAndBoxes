55769f9a36160a3143b5160267edb128d37058b4
janneszuend <zuendja1@students.zhaw.ch>
2024-10-15 11:00:47 +0200

    finished lab

--- labs/lab05/app/src/main/java/com/example/lab05/MainActivity.kt
+++ labs/lab05/app/src/main/java/com/example/lab05/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab05.ui.theme.Lab05Theme
-import groovyjarjarpicocli.CommandLine.Help.Column
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +30,7 @@ class MainActivity : ComponentActivity() {
             Lab05Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column {
-                        Greeting(
-                            name = "Android",
+                        StartPage(
                             modifier = Modifier.padding(innerPadding)
                         )
                     }
@@ -34,17 +41,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun StartPage(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Image(
+            painterResource(id = R.drawable.mountain),
+            contentDescription = "Mountain with a bit of snow",
+            contentScale = ContentScale.FillBounds,
+            modifier = modifier.matchParentSize()
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.matchParentSize().padding(24.dp)
+        ) {
+            TextElements("Sunny", 16, FontWeight.Bold)
+            TextElements("Sedrun", 16, fontStyle = FontStyle.Italic)
+            TextElements("12.1°C", 96)
+//            Text("Sunny", fontSize = 16.sp, fontWeight = FontWeight.Bold, color = androidx.compose.ui.graphics.Color.White)
+//            Text("Sedrun", fontSize = 16.sp, fontStyle = FontStyle.Italic, color = androidx.compose.ui.graphics.Color.White)
+//            Text("12.1°C", fontSize = 96.sp, color = androidx.compose.ui.graphics.Color.White)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Lab05Theme {
-        Greeting("Android")
-    }
+fun TextElements(text: String, fontSize : Int, fontWeight: FontWeight = FontWeight.Normal, fontStyle: FontStyle = FontStyle.Normal){
+    Text(text = text, fontSize = fontSize.sp, fontWeight = fontWeight, color = androidx.compose.ui.graphics.Color.White, fontStyle = fontStyle)
 }
