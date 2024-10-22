52904b7d39b791701fa3ccfc63b198c4b0fbe7d3
janneszuend <zuendja1@students.zhaw.ch>
2024-10-22 10:32:55 +0200

    lab06

--- labs/lab05/app/build.gradle.kts
+++ labs/lab05/app/build.gradle.kts
@@ -57,4 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation (libs.volley)
+    implementation (libs.klaxon)
+
 }
---
+++ labs/lab05/app/src/main/java/com/example/lab05/BackingObjects.kt
@@ -0,0 +1,5 @@
+package com.example.lab05
+
+class Weatherboard(val weatherboad: MutableList<WeatherboardEntry>) {}
+
+class WeatherboardEntry(val temperature: Float, val weathercode: Int) {}
