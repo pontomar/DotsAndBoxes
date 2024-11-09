6dba86d939dfdb0c146347e4c2dc1b2dd9c52535
janneszuend <zuendja1@students.zhaw.ch>
2024-11-09 13:37:53 +0100

    first draft of GameBoard

--- DotsAndBoxesApplication/app/src/main/java/com/example/dotsandboxes/SinglePlayer.kt
+++ DotsAndBoxesApplication/app/src/main/java/com/example/dotsandboxes/SinglePlayer.kt
@@ -2,7 +2,6 @@ package com.example.dotsandboxes
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
