3c04bd259b36b80cfaffbee8726a4903aff85402
janneszuend <zuendja1@students.zhaw.ch>
2024-11-11 20:29:04 +0100

    refactoring

--- DotsAndBoxesApplication/app/src/main/java/com/example/dotsandboxes/HelperComponents.kt
+++ DotsAndBoxesApplication/app/src/main/java/com/example/dotsandboxes/HelperComponents.kt
@@ -3,12 +3,15 @@ package com.example.dotsandboxes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -40,4 +43,46 @@ fun GameButton(
     Button(onClick = onClick, modifier = modifier.fillMaxWidth()) {
         Text(text, fontSize = 25.sp)
     }
+}
+
+@Composable
+fun PopUpInfoForUser(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Example Icon")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
 }
