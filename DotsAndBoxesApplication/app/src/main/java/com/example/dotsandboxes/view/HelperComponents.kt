7d6bdd15fd11a3d983684e90a651f17165279648
ramaflow <pontonglang@protonmail.ch>
2024-11-26 21:11:44 +0100

    cleanup

--- DotsAndBoxesApplication/app/src/main/java/com/example/dotsandboxes/view/HelperComponents.kt
+++ DotsAndBoxesApplication/app/src/main/java/com/example/dotsandboxes/view/HelperComponents.kt
@@ -2,20 +2,17 @@ package com.example.dotsandboxes.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
